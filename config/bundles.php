7e8733ad483c5c60b8d39f9d1a2e5c3811303357
coraline9d <120565448+coraline9d@users.noreply.github.com>
2024-03-28 15:21:21 +0100

    installation of webpack done

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
