65cc03e0dbcc095e295c72146fe380b5bcdb0ffb
coraline9d <120565448+coraline9d@users.noreply.github.com>
2024-03-30 14:10:32 +0100

    totalcost process done

--- assets/scripts/ajax.js
+++ assets/scripts/ajax.js
@@ -52,6 +52,9 @@ document.addEventListener("DOMContentLoaded", function () {
     button.addEventListener("click", function (event) {
       event.preventDefault();
       if (confirm("Êtes-vous sûr de vouloir supprimer cette réservation ?")) {
+        const reservationRow = button.parentElement.parentElement;
+        const totalCostElement = document.querySelector("#total-cost");
+
         fetch(button.getAttribute("href"), {
           method: "POST",
           headers: {
@@ -68,7 +71,25 @@ document.addEventListener("DOMContentLoaded", function () {
           })
           .then((data) => {
             if (data.status === "success") {
-              button.parentElement.parentElement.remove();
+              reservationRow.remove(); // Supprimez la ligne de réservation de la table
+              // Mettez à jour le prix total affiché sur la page avec le nouveau prix total reçu de la réponse
+              totalCostElement.textContent = data.totalCost;
+              // Appel de la méthode totalCost pour recalculer le prix total après la suppression
+              fetch("/reservation/total-cost")
+                .then((response) => response.json())
+                .then((data) => {
+                  // Accédez à la propriété totalCost de l'objet JSON
+                  let totalCost = data.totalCost;
+                  // Mettez à jour le coût total affiché sur la page avec le nouveau prix total
+                  totalCostElement.textContent = totalCost;
+                })
+                .catch((error) => {
+                  console.error(
+                    "Une erreur s'est produite lors du calcul du prix total :",
+                    error
+                  );
+                  // Affichez une alerte ou un message d'erreur en cas d'échec du calcul du prix total
+                });
             } else {
               alert("Une erreur s'est produite");
             }
--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -19,67 +19,67 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ReservationController extends AbstractController
 {
     #[Route('/', name: 'app_reservation_index', methods: ['GET'])]
-public function index(ReservationRepository $reservationRepository): Response
-{
-    // Récupération de toutes les réservations non payées de la base de données
-    $reservations = $reservationRepository->findBy(['isPaid' => false]);
+    public function index(ReservationRepository $reservationRepository): Response
+    {
+        // Récupération de toutes les réservations non payées de la base de données
+        $reservations = $reservationRepository->findBy(['isPaid' => false]);
 
-    return $this->render('reservation/index.html.twig', [
-        'reservations' => $reservations,
-    ]);
-}
-
-    #[Route('/new/{offerId?}', name: 'app_reservation_new', methods: ['GET', 'POST'])]
-public function new(Request $request, EntityManagerInterface $entityManager, OfferRepository $offerRepository, $offerId = null): Response
-{
-    // Récupération de l'utilisateur connecté
-    $user = $this->getUser();
-
-    if ($offerId === null) {
-        // Redirigez l'utilisateur vers la page de sélection d'une offre
-        return $this->redirectToRoute('app_offer');
-    } 
-
-    // Récupération de l'offre sélectionnée
-    $offer = $offerRepository->find($offerId);
-
-    $reservation = new Reservation();
-    
-    // Pré-remplissage du nom et du prénom de l'utilisateur
-    $reservation->setFirstname($user->getFirstname());
-    $reservation->setLastname($user->getLastname());
-     
-// Création d'une collection avec l'offre
-$offers = new ArrayCollection();
-$offers->add($offer);
-
-// Ajout des offres à la réservation
-foreach ($offers as $offer) {
-    $reservation->addOffer($offer);
-}
-
-
-$form = $this->createForm(ReservationType::class, $reservation, [
-    'selected_offer' => [$offer], // Passer l'offre sélectionnée comme une option au formulaire
-]);
-;
-     $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        // Association de l'utilisateur à la réservation
-        $reservation->setUser($user);
-
-        $entityManager->persist($reservation);
-        $entityManager->flush();
-
-        return $this->redirectToRoute('app_reservation_index', [], Response::HTTP_SEE_OTHER);
+        return $this->render('reservation/index.html.twig', [
+            'reservations' => $reservations,
+        ]);
     }
 
-    return $this->render('reservation/new.html.twig', [
-        'reservation' => $reservation,
-        'form' => $form->createView(),
-    ]);
-}
+    #[Route('/new/{offerId?}', name: 'app_reservation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, OfferRepository $offerRepository, $offerId = null): Response
+    {
+        // Récupération de l'utilisateur connecté
+        $user = $this->getUser();
+
+        if ($offerId === null) {
+           // Redirigez l'utilisateur vers la page de sélection d'une offre
+           return $this->redirectToRoute('app_offer');
+        } 
+
+        // Récupération de l'offre sélectionnée
+        $offer = $offerRepository->find($offerId);
+
+        $reservation = new Reservation();
+    
+        // Pré-remplissage du nom et du prénom de l'utilisateur
+        $reservation->setFirstname($user->getFirstname());
+        $reservation->setLastname($user->getLastname());
+     
+        // Création d'une collection avec l'offre
+        $offers = new ArrayCollection();
+        $offers->add($offer);
+
+        // Ajout des offres à la réservation
+        foreach ($offers as $offer) {
+            $reservation->addOffer($offer);
+        }
+
+
+        $form = $this->createForm(ReservationType::class, $reservation, [
+        'selected_offer' => [$offer], // Passer l'offre sélectionnée comme une option au formulaire
+        ]);;
+     
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Association de l'utilisateur à la réservation
+            $reservation->setUser($user);
+
+            $entityManager->persist($reservation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_reservation_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('reservation/new.html.twig', [
+            'reservation' => $reservation,
+            'form' => $form->createView(),
+        ]);
+    }
 
     #[Route('/{id}', name: 'app_reservation_show', methods: ['GET'])]
     public function show(Reservation $reservation): Response
@@ -108,59 +108,60 @@ $form = $this->createForm(ReservationType::class, $reservation, [
     }
     
     #[Route('/{id}', name: 'app_reservation_delete', methods: ['POST'])]
-    public function delete(Request $request, Reservation $reservation, EntityManagerInterface $entityManager): Response
-    {
-        // Vérification du jeton CSRF
-        if (!$this->isCsrfTokenValid('delete'.$reservation->getId(), $request->request->get('_token'))) {
-            // Retourner une réponse d'erreur ou rediriger si le jeton CSRF est invalide
-            // Par exemple, return new Response('Invalid CSRF Token', Response::HTTP_BAD_REQUEST);
-            // ou return $this->redirectToRoute('app_reservation_index');
-        }
-    
-        // Début de la transaction
-        $entityManager->beginTransaction();
-        try {
-            // Suppression des offres associées à la réservation
-            foreach ($reservation->getOffer() as $offer) {
-                $reservation->removeOffer($offer);
-                // Pas besoin de persister l'offre ici car elle sera automatiquement supprimée de la base de données
-            }
-    
-            // Suppression de la réservation
-            $entityManager->remove($reservation);
-            $entityManager->flush(); // Exécution de la transaction
-    
-            // Validation de la transaction
-            $entityManager->commit();
-    
-            // Retourner une réponse JSON indiquant le succès de la suppression
-            return new JsonResponse(['status' => 'success']);
-        } catch (\Exception $e) {
-            // En cas d'erreur, annuler la transaction et renvoyer une réponse d'erreur
-            $entityManager->rollback();
-            return new JsonResponse(['status' => 'error', 'message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-    }
-
-#[Route('/total-cost', name: 'reservation_total_cost')]
-public function getTotalCost(ReservationRepository $reservationRepository): Response
+public function delete(Request $request, Reservation $reservation, EntityManagerInterface $entityManager, ReservationRepository $reservationRepository): Response
 {
-    // Récupérez toutes les réservations non payées
-    $reservations = $reservationRepository->findBy(['isPaid' => false]);
-
-    $totalCost = 0;
-
-    // Calculez le coût total
-    foreach ($reservations as $reservation) {
-        foreach ($reservation->getOffer() as $offer) {
-            $totalCost += $offer->getPrice() * $reservation->getNumberOfTicket();
-        }
+    // Vérification du jeton CSRF
+    if (!$this->isCsrfTokenValid('delete'.$reservation->getId(), $request->request->get('_token'))) {
+        // Retourner une réponse d'erreur ou rediriger si le jeton CSRF est invalide
+        // Par exemple, return new Response('Invalid CSRF Token', Response::HTTP_BAD_REQUEST);
+        // ou return $this->redirectToRoute('app_reservation_index');
     }
 
-    // Renvoyez le coût total en tant que réponse JSON
-    return new JsonResponse($totalCost);
+    // Début de la transaction
+    $entityManager->beginTransaction();
+    try {
+        // Suppression des offres associées à la réservation
+        foreach ($reservation->getOffer() as $offer) {
+            $reservation->removeOffer($offer);
+            // Pas besoin de persister l'offre ici car elle sera automatiquement supprimée de la base de données
+        }
+
+        // Suppression de la réservation
+        $entityManager->remove($reservation);
+        $entityManager->flush(); // Exécution de la transaction
+
+        // Appel de la méthode getTotalCost pour obtenir le coût total après la suppression
+        $totalCost = $this->getTotalCost($reservationRepository)->getContent();
+
+        // Validation de la transaction
+        $entityManager->commit();
+
+        // Retourner une réponse JSON indiquant le succès de la suppression et le coût total
+        return new JsonResponse(['status' => 'success', 'totalCost' => $totalCost]);
+    } catch (\Exception $e) {
+        // En cas d'erreur, annuler la transaction et renvoyer une réponse d'erreur
+        $entityManager->rollback();
+        return new JsonResponse(['status' => 'error', 'message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
 }
 
 
+    #[Route('/total-cost', name: 'reservation_total_cost', methods: ['GET'])]
+    public function getTotalCost(ReservationRepository $reservationRepository): Response
+    {
+        // Récupérez toutes les réservations non payées
+        $reservations = $reservationRepository->findBy(['isPaid' => false]);
 
+        $totalCost = 0;
+
+        // Calculez le coût total
+        foreach ($reservations as $reservation) {
+            foreach ($reservation->getOffer() as $offer) {
+                $totalCost += $offer->getPrice() * $reservation->getNumberOfTicket();
+            }
+        }
+
+        // Renvoyez le coût total en tant que réponse JSON
+        return new JsonResponse($totalCost);
+    }
 }
